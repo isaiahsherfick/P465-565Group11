a2c6cef7c1cdec6344a113c6cf2bd6ce7f1a911d
Isaiah <isaiahsherfick8@gmail.com>
2020-10-01 19:38:36 -0400

    Re-structure

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import os
-flask_app = Flask(__name__, static_folder='../build',static_url_path='/')
+flask_app = Flask(__name__, static_folder='./build',static_url_path='/')
 @flask_app.route("/")
 def hello():
 		return flask_app.send_static_file('index.html')
