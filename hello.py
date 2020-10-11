77c54a342d7382dbb2c5ea3377b79bb137a5b86b
Sanyam Rajpal <rajpalsanyam1997@gmail.com>
2020-10-11 19:02:01 -0400

    editing to code for testing

--- hello.py
+++ hello.py
@@ -83,20 +83,29 @@ db = SQLAlchemy( app )
 
 @app.route( '/' , methods = [ 'GET' , 'POST' ] )
 def index(  ) :
-    if request.method == 'POST' :
-        form = request.form
-        nm = form[ 'name' ]
-        unm = form[ 'username' ]
-        # pw = form[ 'password' ]
-        # age = form[ 'age' ]
-        ids = len( connection.execute( 'SELECT * FROM users;' ).fetchall( ) )
-        # connection.execute( 'INSERT INTO users(id,name,username) VALUES(1, name,username);' , ( name0 , username0 ) )
-        connection.execute( "INSERT INTO users(id,name,username,password) VALUES(%s,%s,%s,%s)" , ( ids+1 , nm , unm , "Pass@1" ) )
-        # connection.execute('INSERT INTO users(id,name,username) VALUES(1,'Isaiah','Sherfick');')
-        # connection.commit( )
-        # db.connection.commit( )
-        return "success"
-    return app.send_static_file('index.html')
+#     if request.method == 'POST' :
+#         form = request.form
+#         nm = form[ 'name' ]
+#         unm = form[ 'username' ]
+#         # pw = form[ 'password' ]
+#         # age = form[ 'age' ]
+#         ids = len( connection.execute( 'SELECT * FROM users;' ).fetchall( ) )
+#         # connection.execute( 'INSERT INTO users(id,name,username) VALUES(1, name,username);' , ( name0 , username0 ) )
+#         connection.execute( "INSERT INTO users(id,name,username,password) VALUES(%s,%s,%s,%s)" , ( ids+1 , nm , unm , "Pass@1" ) )
+#         # connection.execute('INSERT INTO users(id,name,username) VALUES(1,'Isaiah','Sherfick');')
+#         # connection.commit( )
+#         # db.connection.commit( )
+#         return "success"
+#     return app.send_static_file('index.html')
+
+    first_name = request.get_json()['name']
+    email = request.get_json()['email']
+    #password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
+    created = datetime.utcnow()
+    ids = len( connection.execute( 'SELECT * FROM users;' ).fetchall( ) )
+    connection.execute( 'INSERT INTO users(id,name,username,password) VALUES(%s,%s,%s,%s);' , ( ids+1, first_name,email,"Pass@1" ) )
+    result = {'email': email + ' registered'}
+    return jsonify({'result' : result})
 
 @app.route( '/employees' )
 def employees( ) :
