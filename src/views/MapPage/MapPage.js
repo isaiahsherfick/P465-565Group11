7f45aef062a3f3185b1259155a8c3ba47cbb92ee
Robert Lucas <rclucas@iu.edu>
2020-10-13 00:57:12 -0400

    Update MapPage.js
    Changed MapBox API to Google Maps API and added search functions.

--- src/views/MapPage/MapPage.js
+++ src/views/MapPage/MapPage.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import Input from 'components/CustomInput/CustomInput.js'
+import React from 'react';
+import './search.css'
+//import Input from 'components/CustomInput/CustomInput.js'
 
 const mapboxURL = 'https://api.mapbox.com/styles/v1/sneakbots/ckfg8aias0fui19s07ubr19sx.html?fresh=true&title=view&access_token=pk.eyJ1Ijoic25lYWtib3RzIiwiYSI6ImNrZmZzajh2azBmMnUydG83ZHlteDVqZGQifQ.ekQc9lMhLZop23CsTobsPA#11.09/33.7785/-118.256'
 
-const MapPage = () => { 
+const MapPage = () => {
     return (
         <div>
             <div className="iframe">
-                <iframe src={mapboxURL} width="100%" height="100%"></iframe>
+            <iframe
+
+  frameborder="0" 
+  style={{border:0}}
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDAwFRjZJPeT3I9R4nGBap0gJorBu9aHLc
+    &q=Space+Needle,Seattle+WA" allowfullscreen>
+</iframe>
             </div>
-            <div className="search-bar">
-                <Input labelText="Search" />
-            </div>
+            <form onsubmit="event.preventDefault();" role="search" className="form">
+                <label for="search">Search for stuff</label>
+                <input id="search" type="search" placeholder="Search..." autofocus required />
+                <button type="submit">Go</button>
+            </form> 
         </div>
     )
+
 }
 
 export default MapPage
