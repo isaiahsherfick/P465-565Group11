0439348ce1f1fa172d449ab394608bf13be7ad8e
Robert Lucas <rclucas@iu.edu>
2020-10-14 12:23:18 -0400

    Update MapPage.js
    partial update for search bar. still has bugs.

--- src/views/MapPage/MapPage.js
+++ src/views/MapPage/MapPage.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import './search.css'
 //import Input from 'components/CustomInput/CustomInput.js'
+import axios from "axios";
 
 const mapboxURL = 'https://api.mapbox.com/styles/v1/sneakbots/ckfg8aias0fui19s07ubr19sx.html?fresh=true&title=view&access_token=pk.eyJ1Ijoic25lYWtib3RzIiwiYSI6ImNrZmZzajh2azBmMnUydG83ZHlteDVqZGQifQ.ekQc9lMhLZop23CsTobsPA#11.09/33.7785/-118.256'
 
 const MapPage = () => {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: null,
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.onChange =this.onChange.bind(this)
+    }
+
+
+      handleSubmit(event){
+        event.preventDefault();
+        const user = {
+          name: this.state.name
+        } 
+        axios.post('https://jsonplaceholder.typicode.com/users', { user })
+          .then(res=>{
+            console.log(res);
+            console.log(res.data);
+            window.location = "/retrieve" //This line of code will redirect you once the submission is succeed
+          })
+      }
+
+
+
+    handleChange(event){
+        this.setState({ name: event.target.value});
+      }
+    
+
     return (
         <div>
             <div className="iframe">
@@ -12,13 +45,14 @@ const MapPage = () => {
 
   frameborder="0" 
   style={{border:0}}
-  src="https://www.google.com/maps/embed/v1/place?key=API_KEY
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDAwFRjZJPeT3I9R4nGBap0gJorBu9aHLc
     &q=Space+Needle,Seattle+WA" allowfullscreen>
 </iframe>
             </div>
             <form onsubmit="event.preventDefault();" role="search" className="form">
                 <label for="search">Search for stuff</label>
                 <input id="search" type="search" placeholder="Search..." autofocus required />
+                <input type = "text" name = "name" onChange= {this.handleChange}/>
                 <button type="submit">Go</button>
             </form> 
         </div>
