bb138398d272e92629dece8488d8867d2d29d2f7
isaiahsherfick <isaiahsherfick8@gmail.com>
2020-10-14 19:18:12 -0400

    Modified again P565F19P11-110

--- flights.sql
+++ flights.sql
@@ -7,21 +7,17 @@ CREATE TABLE public.flights
     takeofftime timestamp with time zone,
     takeoffdate date NOT NULL,
     startcity character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    "isTwoWay" integer NOT NULL,
     id integer NOT NULL,
-    firstclassrate double precision,
     endcity character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    coachrate timestamp with time zone,
     businessrate double precision,
     arrivaldate date NOT NULL,
     airline character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    arrivaltime timestamp without time zone[],
+    coachrate double precision NOT NULL,
+    arrivaltime timestamp with time zone NOT NULL,
     CONSTRAINT flights_pkey PRIMARY KEY (id)
-	--table seems overcomplicated, room for refactoring later. For now, everything seems necessary.
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE public.flights
     OWNER to ugcuvkvpcdaixu;
-
