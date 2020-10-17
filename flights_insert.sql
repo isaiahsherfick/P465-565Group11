c9d16c9e37df657e91fbcdd882da190316e5aa50
isaiahsherfick <isaiahsherfick8@gmail.com>
2020-10-17 00:08:58 -0400

    Added image url column to DB P565F19P11-114

--- flights_insert.sql
+++ flights_insert.sql
@@ -1,2 +1,3 @@
 insert into flights values('2020-10-15 12:00-04', 'Indianapolis', 0, 'Los Angeles', 300.00, 'Southwest', 150.00, '2020-10-15 16:00-07');
+insert into flights values('2020-10-15 12:00-07', 'Los Angeles', 1, 'Chicago', 300.00, 'Southwest', 150.00, '2020-10-15 16:00-05');
 --sample flight
