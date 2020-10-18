7eb1afc7d3c9c6c8aaa970fc8b4464d2835d0bfc
Robert Lucas <rclucas@iu.edu>
2020-10-18 17:05:01 -0400

    Create Header2.js

---
+++ src/Header2.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header2.scss'
+
+const Header2 = () => {
+    return (
+  <nav className="nav2">
+    <ul class="menuItems">
+      <li><a href='#' data-item='TravelAdvice'>Travel Advice</a></li>
+      <li><a href='#' data-item='Hotels'>Hotels</a></li>
+      <li><a href='#' data-item='TTD'>Things to do</a></li>
+      <li><a href='#' data-item='restaurants'>restaurants</a></li>
+      <li><a href='#' data-item='More'>More</a></li>
+    </ul>
+  </nav>
+
+    )
+}
+
+export default Header2
