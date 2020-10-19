fdbdb3ab9b85d960ed4bc01af8c49dfd7f0c5d5b
rasmitha-koduru <rasmithachoudary@gmail.com>
2020-10-19 14:25:57 -0400

    P565F19P11-91

--- src/components/elements/MapPage.js
+++ src/components/elements/MapPage.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import Header from "./Header";
 import { Link } from 'react-router-dom';
 import { setExploreData } from '../../helpers/common';
+import Autocomplete from './Autocomplete';
 
 
 const mapboxURL = 'https://api.mapbox.com/styles/v1/sneakbots/ckfg8aias0fui19s07ubr19sx.html?fresh=true&title=view&access_token=pk.eyJ1Ijoic25lYWtib3RzIiwiYSI6ImNrZmZzajh2azBmMnUydG83ZHlteDVqZGQifQ.ekQc9lMhLZop23CsTobsPA#11.09/33.7785/-118.256'
@@ -62,12 +63,26 @@ export default class MapPage extends React.Component{
       &q=Space+Needle,Seattle+WA" allowfullscreen="true">
   </iframe>
               </div>
-              <form  role="search" className="form">
+              {/* <form  role="search" className="form">
                   <label for="search">Search for stuff</label>
                   <input id="search" type="search" name = "name" onChange= {this.handleChange} placeholder="Search..." autofocus required />
                   {/* <input type = "text" name = "name" onChange= {this.handleChange}/> */}
-                  <button type="submit" onClick={this.handleSubmit}>Go</button>
-              </form> 
+                  {/* <button type="submit" onClick={this.handleSubmit}>Go</button>
+              </form>  */} 
+              <Autocomplete
+        suggestions={[
+          "Chicago",
+          "Atlanta",
+          "Bloomington",
+          "New York",
+          "Boston",
+          "Seattle",
+          "Houston",
+          "San Jose",
+          "San Francisco",
+          "Los Angeles"
+        ]}
+      />
           </div>
       );
     }
