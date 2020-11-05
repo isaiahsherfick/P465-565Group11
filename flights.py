f1c8f44dd7368c8a39db055dc0bdf2ad23d28b7b
sanyam1997 <rajpalsanyam1997@gmail.com>
2020-11-05 15:53:02 -0500

    # gettting the placeid for the starting city

--- flights.py
+++ flights.py
@@ -1,33 +1,77 @@
 # importing necessary libraries
-from collections import defaultdict
-from flask import jsonify
-from abc import abstractmethod
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-import datetime , pytz
-import requests
+import json , requests
+from datetime import datetime
 
 # Adding the Heroku URL
 DB_URL = 'postgres://ugcuvkvpcdaixu:b624b6193c9e248af602f7239c6ddca6848239242adbcb31a9fd4685ac75aabf@ec2-204-236-228-169.compute-1.amazonaws.com:5432/d93me5889f2sp1'
 # Building a connection to the database
 engine = create_engine( DB_URL )
 
-
-
-
-
-url = "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
-
-querystring = {"query":"Chicago"}
-
+# the url for the flighscanners api
+# and the headers with key fed into it
+# url = "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
 headers = {
     'x-rapidapi-key': "bd4910c4f3msh6daf741cb57f839p19efffjsn9bbea2015d64",
     'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+class Flights( ) :
 
-print(response.text)
+    def __init__( self , startCity = '' , destinationCity = '' , startDate = datetime.today( ).strftime( '%Y-%m-%d' ) , endDate = "" ) :
+        self.startCity = startCity
+        self.destinationCity = destinationCity
+        self.startDate = startDate
+        self.endDate = endDate
 
+    # getting the start city
+    def getStartCity( self ) :
+        return self.startCity
 
+    # setting the start city
+    def setStartCity( self , startCity ) :
+        self.startCity = startCity
 
+    # getting the destionation city
+    def getDestinationCity( self ) :
+        return self.destinationCity
+
+    # setting the destionation city
+    def setDestinationCity( self , destinationCity ) :
+        self.destinationCity = destinationCity
+
+    # getting the start date
+    def getStartDate( self ) :
+        return self.startDate
+
+    # setting the start date
+    def setStartDate( self , startDate ) :
+        self.startDate = startDate
+
+    # getting the end date
+    def getEndDate( self ) :
+        return self.endDate
+
+    # setting the start date
+    def setEndDate( self , endDate ) :
+        self.endDate = endDate
+
+    # the function to call the skyscanner's api
+    def populatePortsFromAPI( self ) :
+        # calling engine connect only temporarily so that it gets disconnected thereafter and doesn't remain always connected
+        with engine.connect( ) as connection :
+            # gettting the response for the starting city
+            startResponse = requests.request( "GET" , url , headers = headers , params = { "query" : self.startCity } ).json( )
+            # gettting the response for the destination city
+            destinationResponse = requests.request( "GET" , url , headers = headers , params = { "query" : self.destinationCity } ).json( )
+            # gettting the placeid for the starting city
+            startPlace = startResponse[ "Places" ][ 0 ][ "PlaceId" ]
+            # gettting the placeid for the ending city
+            destinationPlace = destinationResponse[ "Places" ][ 0 ][ "PlaceId" ]
+            # generating the url with start and end city and generating the url
+            # with all the parameters and the key fed into it
+            url2 = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + startPlace + "/" + destinationPlace + "/" + self.startDate + "/" + self.endDate + "?rapidapi-key=" \
+            + headers[ "x-rapidapi-key" ]
+            # getting the request for the url and showing the json outptu which will be used thereafter
+            print( requests.request( "GET" , url2 , headers = headers ).json( ) )
--- flights_test.py
+++ flights_test.py
@@ -1,63 +1,12 @@
-# importing necessary libraries
+# the testing file for flights
 from flights import *
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from collections import defaultdict
 
-#
-flight_details = defaultdict( list )
-flight_details[ 'takeOffTime' ] = '2020-10-15 16:00:00+00:00'
-flight_details[ 'startCity' ] = 'Indianapolis'
-flight_details[ 'endCity' ] = 'Los Angeles'
-flight_details[ 'businessRate' ] = 300.00
-flight_details[ 'airline' ] = 'Southwest'
-flight_details[ 'coachRate' ] = 150.00
-flight_details[ 'arrivalTime' ] = '2020-10-15 23:00:00+00:00'
+# generating an object for the input
+flights_object = Flights( 'Los Angeles' , 'New York' )
 
-testFlightToLA = Flight( flight_details )
-
-# testing getters functions
-print( testFlightToLA.getTakeOffTime( ) )
-print( testFlightToLA.getStartCity( ) )
-print( testFlightToLA.getEndCity( ) )
-print( testFlightToLA.getBusinessRate( ) )
-print( testFlightToLA.getAirline( ) )
-print( testFlightToLA.getCoachRate( ) )
-print( testFlightToLA.getArrivalTime( ) )
-
-# testing setters functions
-flight_details[ 'takeOffTime' ] = '2020-10-15 16:00:00+00:00'
-testFlightToLA.setStartCity( 'Indianapolis' )
-testFlightToLA.setEndCity( 'Los Angeles' )
-testFlightToLA.setBusinessRate( 300.00 )
-testFlightToLA.setAirline( 'Southwest' )
-testFlightToLA.setCoachRate( 150.00 )
-flight_details[ 'arrivalTime' ] = '2020-10-15 23:00:00+00:00'
-
-
-# testing getters functions
-print("---------------------output for testFlightToLA--------------------------")
-print( testFlightToLA.getTakeOffTime( ) )
-print( testFlightToLA.getStartCity( ) )
-print( testFlightToLA.getEndCity( ) )
-print( testFlightToLA.getBusinessRate( ) )
-print( testFlightToLA.getAirline( ) )
-print( testFlightToLA.getCoachRate( ) )
-print( testFlightToLA.getArrivalTime( ) )
-print("---------------------output for testFlightToLA--------------------------")
-
-# intiailizing the datetime
-testFlightFromDB = Flight( )
-testFlightFromDB.initFromDatabase( 0 )
-print("---------------------output for testFlightFromDB-------------------------")
-print(testFlightFromDB.getTakeOffTime( ) )
-print( testFlightFromDB.getStartCity( ) )
-print( testFlightFromDB.getEndCity( ) )
-print( testFlightFromDB.getBusinessRate( ) )
-print( testFlightFromDB.getAirline( ) )
-print( testFlightFromDB.getCoachRate( ) )
-print( testFlightFromDB.getArrivalTime( ) )
-print("---------------------output for testFlightFromDB-------------------------")
-
-# verificationt that the testing function is querying the database correctly as the data we have fed in
-print( testFlightFromDB.equals( testFlightToLA ) )
+# printing the getters
+print( flights_object.getStartCity( ) )
+print( flights_object.getDestinationCity( ) )
+# calling the function which calls skyscanner api which in turn uses that data for generating flights itinerary
+flights_object.populatePortsFromAPI( )
+# print( flights_object.producePortsJson( ) )
