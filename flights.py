e85f654c8e89dd97ca3b20ca351ed1db23248491
isaiahsherfick <isaiahsherfick8@gmail.com>
2020-11-05 14:04:36 -0500

    Started flights class  P565F19P11-137

--- flights.py
+++ flights.py
@@ -5,6 +5,7 @@ from abc import abstractmethod
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 import datetime , pytz
+import requests
 
 # Adding the Heroku URL
 DB_URL = 'postgres://ugcuvkvpcdaixu:b624b6193c9e248af602f7239c6ddca6848239242adbcb31a9fd4685ac75aabf@ec2-204-236-228-169.compute-1.amazonaws.com:5432/d93me5889f2sp1'
@@ -13,105 +14,20 @@ engine = create_engine( DB_URL )
 
 
 
-class Flight :
-    def __init__( self , itinerary = defaultdict( list ) ) :
-        self.takeOffTime = itinerary[ 'takeOffTime' ]
-        self.startCity = itinerary[ 'startCity' ]
-        self.endCity = itinerary[ 'endCity' ]
-        self.businessRate = itinerary[ 'businessRate' ]
-        self.airline = itinerary[ 'airline' ]
-        self.coachRate = itinerary[ 'coachRate' ]
-        self.arrivalTime = itinerary[ 'arrivalTime' ]
 
-    # getters
-    # returning the takeOffTime
-    def getTakeOffTime( self ) :
-        return self.takeOffTime
 
-    # returning the startCity
-    def getStartCity( self ) :
-        return self.startCity
+url = "https://rapidapi.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/"
 
-    # returning the endCity
-    def getEndCity( self ) :
-        return self.endCity
+querystring = {"query":"Chicago"}
 
-    # returning the businessRate
-    def getBusinessRate( self ) :
-        return self.businessRate
+headers = {
+    'x-rapidapi-key': "bd4910c4f3msh6daf741cb57f839p19efffjsn9bbea2015d64",
+    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+    }
 
-    # returning the airline
-    def getAirline( self ) :
-        return self.airline
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-    # returning the coachRate
-    def getCoachRate( self ) :
-        return self.coachRate
+print(response.text)
 
-    # returning the arrivalTime
-    def getArrivalTime( self ) :
-        return self.arrivalTime
 
-    # setters
-    # setting the takeOffTime as per input
-    def setTakeOffTime( self , takeOffTime ) :
-        self.takeOffTime = takeOffTime
 
-    # setting the startCity as per input
-    def setStartCity( self , startCity ) :
-        self.startCity = startCity
-
-    # setting the endCity as per input
-    def setEndCity( self , endCity ) :
-        self.endCity = endCity
-
-    # setting the businessRate as per input
-    def setBusinessRate( self , businessRate ) :
-        self.businessRate = businessRate
-
-    # setting the airline as per input
-    def setAirline( self , airline ) :
-        self.airline = airline
-
-    # setting the coachRate as per input
-    def setCoachRate( self , coachRate ) :
-        self.coachRate = coachRate
-
-    # setting the arrivalTime as per input
-    def setArrivalTime( self , arrivalTime ) :
-        self.arrivalTime = arrivalTime
-
-    # intialized the database by accessing the flight details for the respective id
-    def initFromDatabase( self , id ) :
-        connection = engine.connect( )
-        summary = connection.execute( "SELECT * FROM flights WHERE id=(%s);" , ( id ) ).fetchall( )[ -1 ]
-        connection.close( )
-        self.setTakeOffTime( str( summary[ 0 ] ) )
-        self.setStartCity( summary[ 1 ] )
-        self.setEndCity( summary[ 3 ] )
-        self.setBusinessRate( summary[ 4 ] )
-        self.setAirline( summary[ 5 ] )
-        self.setCoachRate( summary[ 6 ]  )
-        self.setArrivalTime( str( summary[ 7 ] ) )
-
-    def reportData( self ) :
-        # making a dictionary of all the features which one maps for all the getter functions
-        report_document = defaultdict( list )
-        report_document[ "TakeOffTime" ] = self.getTakeOffTime( )
-        report_document[ "StartCity" ] = self.getStartCity( )
-        report_document[ "EndCity" ] = self.getEndCity( )
-        report_document[ "BusinessRate" ] = self.getBusinessRate( )
-        report_document[ "Airline" ] = self.getAirline( )
-        report_document[ "CoachRate" ] = self.getCoachRate( )
-        report_document[ "ArrivalTime" ] = self.getArrivalTime( )
-        return report_document
-
-    # verification functions for testing
-    def equals( self , other ) :
-        return self.getTakeOffTime() == other.getTakeOffTime( ) and \
-        self.getStartCity( ) == other.getStartCity( ) and \
-        self.getEndCity( ) == other.getEndCity( ) and \
-        self.getBusinessRate( ) == other.getBusinessRate( ) and \
-        self.getAirline( ) == other.getAirline( ) and \
-        self.getArrivalTime( ) == other.getArrivalTime( ) and \
-        self.getCoachRate( ) == other.getCoachRate( )
