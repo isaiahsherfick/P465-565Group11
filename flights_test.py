891a8f94cf94e6fc79c2bbf5ca8883ea523710b7
sanyam1997 <rajpalsanyam1997@gmail.com>
2020-11-08 12:24:03 -0500

    P565F19P11-137

--- flights_test.py
+++ flights_test.py
@@ -8,5 +8,5 @@ flights_object = Flights( 'Los Angeles' , 'New York' )
 print( flights_object.getStartCity( ) )
 print( flights_object.getDestinationCity( ) )
 # calling the function which calls skyscanner api which in turn uses that data for generating flights itinerary
-flights_object.populatePortsFromAPI( )
+flights_object.productJson( )
 # print( flights_object.producePortsJson( ) )
