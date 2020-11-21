f00cec664824f6b7825c28599beac590940a356b
Robert Lucas <rclucas@iu.edu>
2020-11-21 12:15:38 -0500

    Update Itinerary.js

--- src/components/elements/Itinerary.js
+++ src/components/elements/Itinerary.js
@@ -1,16 +1,15 @@
 import Header2 from './Header2';
 import Header from './Header'
 import React, {useState, useEffect} from 'react'
+import { withRouter } from 'react-router-dom';
 //import React, { useState } from 'react'
-import './Itinerary.css'
+ 
 import { getUsername } from '../../helpers/common';
 //import Header from '../layout/Header';
 
 import { getExploreData } from '../../helpers/common';
-import { Link } from 'react-router-dom';
-import { getUserId } from '../../helpers/common';
 
-
+const LS_DATA = 'ITERNARY';
 const myData =  [
   {userId: "3", hotelName: "maria de france"},
   {userId: "3", hotelName: "maria de spain"},
@@ -20,25 +19,28 @@ const myData =  [
 
 
 
-function Itinerary() {
+function Itinerary(props) {
 
  const [myData, setMyData] =  useState([])
- const [myComment, setMyComment] = useState([])
 
   useEffect(() => {
-    fetch('https://roadmappr.herokuapp.com/retrieveItinerary', {
-      method: "POST",
-       headers: {
-        "content-type" : "application/json"
-       },
-       body: JSON.stringify({
-         userId: getUserId()
-       })
-    })
-    .then(response => response.json())
-    .then(data => setMyData(data.tasks))
-    .then(comment => setMyComment(comment.comments))
-    .catch(err => console.log(err))
+
+    setMyData(JSON.parse(localStorage.getItem(LS_DATA)));
+    // fetch('https://roadmappr.herokuapp.com/retrieveItinerary', {
+    //   method: "POST",
+    //    headers: {
+    //     "content-type" : "application/json"
+    //    },
+    //    body: JSON.stringify({
+    //      userId: "3"
+    //    })
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //   setMyData(data.tasks)
+    // console.log("RESPONSE DATA===>", data)
+    // })
+    // .catch(err => console.log(err))
   }, [])
   
   console.log(typeof myData)
@@ -52,7 +54,7 @@ const handleDelete =(name) => {
       "content-type" : "application/json"
      },
      body: JSON.stringify({
-        userId: getUserId(),
+        userId: "3",
          name
      })
   })
@@ -62,19 +64,22 @@ const handleDelete =(name) => {
 
 setMyData(myData.filter(data => data !== name))
 }
-
+const reviewHandler = (data)=>{
+props.history.push({
+  pathname: '/reviews',
+  data
+})
+}
+console.log("MY DATA=====>", myData);
 const renderData = (
  
    <div>
-    {myData.map((data) => (
-      <div key={data}>
-         <p> {data}</p>
-          <button onClick={() => handleDelete(data)}>delete </button>
+    {myData.map((data, index) => (
+      <div key={index}>
+         <p>Name: {data.name}</p>
+          <button className="whitetb" onClick={()=>reviewHandler(data)}>Reviews</button>
+          <button onClick={() => handleDelete(data)}>  Delete  </button>
         </div>
-    ))}
-  <p>Here's what others think about your itinerary</p>
-{myComment.map((comment) => (
-         <p> {comment}</p>
     ))}
      
      </div>
@@ -87,18 +92,17 @@ const renderData = (
 
   return (
     <div className="container">
-      <Header />
       <Header2 />
       <h1> Welcome to your Itinerary, {getUsername()} </h1>
 
-{myData.length < 1 ? <p>oops! no itinerary </p> : renderData}
-    <div>Finalize your Itinerary by booking <Link to="/book">here</Link></div>
-    <div>Comment on other itineraries by clicking <Link to="/comment">here</Link></div>
+{myData.length < 1 ? <p>oops! no itenerary </p> : renderData}
 
    
+
     </div>
   )
 
 }
 
-export default Itinerary;
+export default withRouter(Itinerary);
+
