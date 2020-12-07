32f954a9384cae3a85230bca618f409d9e699339
Robert Lucas <rclucas@iu.edu>
2020-12-07 12:13:34 -0500

    Update ViewReviews.js

--- src/components/elements/ViewReviews.js
+++ src/components/elements/ViewReviews.js
@@ -2,9 +2,14 @@ import React , {useState, useEffect} from 'react'
 import Header2 from './Header2';
 import Header from './Header'
 
-const ViewReviews = () => {
+const ViewReviews = (props) => {
 
+  const arr = []
     const [reviews, setReviews] =  useState([])
+
+    const {data} = props.location;
+
+    console.log(data);
 
   useEffect(() => {
     fetch('https://roadmappr.herokuapp.com/gatherReview', {
@@ -13,33 +18,51 @@ const ViewReviews = () => {
         "content-type" : "application/json"
        },
        body: JSON.stringify({
-        unique_location_key: "ChIJkyqbhMvt6IgRi-51ty3Jz-E"
+        unique_location_key: data
        })
     })
     .then(response => response.json())
     .then(data => {
-      console.log(data)
-       setReviews(data)
+     console.log(data.reviews);
+       setReviews(data.reviews)
+       
     }
     )
     .catch(err => console.log(err))
   }, [])
+
+  var myData = Object.keys(reviews).map(key => {
+    return reviews[key];
+  })
+
+//   if(reviews){
+//   console.log(reviews, "<=====reviews");
+//   var myData = Object.keys(reviews).map(key => {
+//     return reviews[key];
+// })
+// console.log(myData, "myData");
+//   }
+
+const displayReviews =  reviews && myData.map((item, index) => (
+  <ul key={index}>
+   <li>{item.reviewBody}</li>
+   <li>{item.reviewHeader}</li>
+   <li>{item.starOutOffFive}</li>
+   </ul>
+ ))
+  
+
+ 
     return (
         <div>
           <Header/>
           <Header2 />
 
-{/* <div>
-  {reviews.map(item => (
-   <ul key={item.id}>
-    <li>{item.reviewBody}</li>
-    <li>{item.reviewHeader}</li>
-    <li>{item.starOutOffFive}</li>
-    </ul>
-  ))
-
-  }
-</div> */}
+ <div>
+   {
+   displayReviews
+  }  
+</div> 
 
 
           {/* reviewBody
