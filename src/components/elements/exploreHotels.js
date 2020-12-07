fa87f28363b97dbb66df7b0367bdff0b9433567a
Robert Lucas <rclucas@iu.edu>
2020-12-07 12:12:51 -0500

    Update exploreHotels.js

--- src/components/elements/exploreHotels.js
+++ src/components/elements/exploreHotels.js
@@ -3,10 +3,11 @@ import Header2 from './Header2';
 import Header from './Header'
 import React, {useState} from 'react'
 import { getExploreData } from '../../helpers/common';
-import './Explore.css'
 import { getUserId } from '../../helpers/common';
 
-const LS_DATA = 'ITERNARY';
+import './Explore.css'
+
+
 
 const Explore = (props) => {
 
@@ -14,13 +15,11 @@ const Explore = (props) => {
     
 
 
-             const [myData, setMyData] = useState(cityData.restaurants)
+             const [myData, setMyData] = useState(cityData.hotels)
 
              let placeId = "" 
              const addJson = (myData) => {
-                 const dataToAdd = {placeId: myData.place_id, name: myData.name};
-                 saveToLocalStorage(dataToAdd);
-           
+
                  placeId = myData.place_id;
                 fetch("https://roadmappr.herokuapp.com/addToItinerary", {
                     method: "POST",
@@ -31,36 +30,26 @@ const Explore = (props) => {
                     body:JSON.stringify({
                         // name:  JSON.stringify(myData.name),
                         name: myData.name,
-                        placeId: placeId,
                         userId: getUserId()
 
                     })
                 })
                 .then(res => res.json())
                 .then(() => alert("Added successfully to Itinerary"))
-                .then(() =>console.log(""))
+                .then(() => console.log())
                 .catch(err => console.log(err, "Unable to Post!"))
                 }
-
-                const saveToLocalStorage = (data) =>{
-           
-                   let myLSData = JSON.parse(localStorage.getItem(LS_DATA)) || [];
-                  myLSData.push(data)
-
-                   localStorage.setItem(LS_DATA, JSON.stringify(myLSData));
-                }
-
-                
 
              
 
              var apiKey = "AIzaSyCqflolF2b4aNNcyQs0XdbcoAFwtby7Muw";      
             //  var str1 = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${placeId}&key=${apiKey}`;
                 
-             var photo_reference_goes_here = cityData.restaurants.results[0]['photos'][0]['photo_reference']
+             var photo_reference_goes_here = cityData.hotels.results[0]['photos'][0]['photo_reference']
              var str1 = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${photo_reference_goes_here}&key=AIzaSyANxvSPyPXr2rMPMSUjCqvBQ6_PS2cOs3Q`; 
         
 console.log(photo_reference_goes_here, "<== city data" )
+
 
 const navigate = (data) => {
     props.history.push({
@@ -78,12 +67,11 @@ const navigate = (data) => {
                     <img style={{display:"unset"}} src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${result.photos[0].photo_reference}&key=AIzaSyANxvSPyPXr2rMPMSUjCqvBQ6_PS2cOs3Q`}/>
 
                          <p>{result.name} </p>
-                         <p>{result.rating}</p>
+                         <p>Rating : {result.rating}</p>
                          <p>{result.vicinity}</p>
                 
                          {/* <p onClick={() => addJson(result)}> <a href={"#"}>Add to Itinerary</a> </p> */}
                          <p><button onClick={() => addJson(result)}>Add to Itinerary</button></p>
-                        
                          <p onClick={() => navigate(result.name)}> <a href={"#"}>View Reviews</a> </p>
 
                          {/* <img src={str1} /> */}
@@ -97,14 +85,14 @@ const navigate = (data) => {
 
    
     return (
-        
-       <div >
-           <Header/>
+        <div>
+            <Header/>
            <Header2 /> 
-        <div class='flex-container'>
-        {renderData}
+        
+       <div class='flex-container'>
 
-        </div>
+           {renderData}
+       </div>
        </div>
     )
 }
